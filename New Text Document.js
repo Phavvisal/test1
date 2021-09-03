a24d8e8b74151ab1a10dbf4423167b2623359e48
Phavvisal <phavvisal16@gmail.com>
2021-09-03 08:57:52 +0700

    First commit

---
+++ New Text Document.js
@@ -0,0 +1,11 @@
+const x = [
+    {id: 1, value: 2}, {id:2, value: 3}, {id:3, value: 4}
+]
+
+const y = [
+    {id: 1, value: 3}, {id: 3, value: 4}
+]
+
+result = [
+    {id: 1, value: 5}, {id: 2, value: 3}, {id: 3, value: 8}
+]
